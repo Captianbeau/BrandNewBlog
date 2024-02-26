52443e8b60affb19f151be4a6b687be0945a17dd
Capti <captianbeau@gmail.com>
2024-02-26 12:49:30 -0700

    I lied. I put the comment routes in the user file I fixed the error and now comments are where theyre supposed to be

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -1,3 +1,31 @@
 //get(all&id), post, put, delete
 const router = require('express').Router();
 const{User, Post, Comment} = require('../../models');
+
+//get all comments
+router.get('/', async (req,res) => {
+    try{
+        const commentData = await Comment.findAll({
+            include:[{model: User}, {model: Post}],
+        });
+        res.statusMessage(200).json(commentData);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//get comment by id
+router.get('/:id', async (req, res) => {
+    try{
+    const commentData = await Comment.findByPk(req.params.id, {
+        include: [{model: User}, {model: Post}],
+    });
+    if(!commentData){
+        res.status(404).json({message:'Comment not found'});
+        return;
+    }
+    res.status(200).json(commentData);
+}catch(err){
+    res.status(500).json(err);
+}
+});
