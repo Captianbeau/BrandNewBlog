fb1d1d374810b2f6e33935d10b2c7a1457937097
Capti <captianbeau@gmail.com>
2024-02-26 12:42:13 -0700

    added GETs to api/post

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -1,3 +1,29 @@
 //get(all&id), post, put, delete
 const router = require('express').Router();
 const{User, Post, Comment} = require('../../models');
+
+router.get('/', async (req,res) => {
+    try{
+        const postData = await Post.findAll({
+            include:[{model: User}, {model: Comment}],
+        });
+        res.statusMessage(200).json(postData);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try{
+    const postData = await Post.findByPk(req.params.id, {
+        include: [{model: User}, {model: Comment}],
+    });
+    if(!postData){
+        res.status(404).json({message:'Post not found'});
+        return;
+    }
+    res.status(200).json(postData);
+}catch(err){
+    res.status(500).json(err);
+}
+});
