a9e0cd3fc1f098c9ad1ed9095467ed084ff494db
Capti <captianbeau@gmail.com>
2024-02-25 19:14:48 -0700

    setup model index

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const Post = require('./Post');
+const Comment = require('./Comment');
+const User = require('./User');
+
+Post.hasMany(Comment,{
+    foreignKey:'post_id',
+    onDelete:'CASCADE',
+});
+
+Comment.belongsTo(Post,{
+    foreignKey:'post_id',
+});
+
+User.hasMany(Post,Comment, {
+    foreignKey:'user_id',
+    onDelete:'CASCADE',
+});
+
+module.exports = {
+    Post,
+    Comment,
+    User
+}
