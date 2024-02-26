448a35e9738c454ad6d3ef8e04f7087b3a2c596f
Capti <captianbeau@gmail.com>
2024-02-26 12:22:49 -0700

    added controllers/index and routed to api folder

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const sequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 const sess = {
-    secret: process.env.DB_SECRET,
+    secret: process.env.SECRET,
     cookie:{},
     resave:false,
     saveUninitialized: true,
