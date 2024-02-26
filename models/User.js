155fbc8608476af097d4ae404e2e648e2a724153
Capti <captianbeau@gmail.com>
2024-02-25 19:00:05 -0700

    added User model code

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const {Model, DataTypes} = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPass){
+        return bcrypt.compareSync(loginPass, this.password);
+    }
+}
+
+User.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            primaryKey:true,
+            autoIncrement:true,
+        },
+        username:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            unique:true,
+        },
+        email: {
+            type:DataTypes.STRING,
+            unique:true,
+            validate:{
+                isEmail:true,
+            },
+        },
+        password: {
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                len:[8],
+            },
+        },
+    },
+    {
+        hooks:{
+            beforeCreate: async (newUser) => {
+                newUser.password = await bcrypt.hash(newUser.password, 10);
+            },
+        },
+        sequelize,
+        timestamps:false,
+        freezeTableName:true,
+        modelName:'user',
+    }
+);
+
+module.exports = User;
