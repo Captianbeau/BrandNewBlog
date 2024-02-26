1c40453b5adedb4d6bb4493cc3b78326d820597c
Capti <captianbeau@gmail.com>
2024-02-26 12:17:11 -0700

    added more code to server should allow for localhost connection

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+//TODO set up handlebars and public filepaths
 const express = require('express');
 const session = require('express-session');
+const path = require('path');
+const handlebars = require('express-handlebars');
+
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
@@ -7,3 +11,23 @@ const sequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+    secret: process.env.DB_SECRET,
+    cookie:{},
+    resave:false,
+    saveUninitialized: true,
+    store: new sequelizeStore({
+        db:sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(()=>{
+    app.listen(PORT, ()=> console.log('ready'))
+});
