115bbdfc0f817863ae0c0bdb799aad7a93346f55
Capti <captianbeau@gmail.com>
2024-03-02 16:53:29 -0700

    added post to the post.js and comment.js

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -30,4 +30,21 @@ router.get('/:id', async (req, res) => {
 }
 });
 
+router.post('/', async (req,res) => {
+    try{
+        const commentData = await Comment.create(req.body);
+        res.status(200).json(commentData);
+    }catch(err){
+        res.status(400).json(err);
+    }
+});
+
+// router.put('/:id', async (req,res) => {
+
+// });
+
+// router.delete('/:id', async (req,res) => {
+
+// });
+
 module.exports = router;
--- controllers/api/user.js
+++ controllers/api/user.js
@@ -2,3 +2,12 @@
 const router = require('express').Router();
 const{User} = require('../../models');
 
+// router.post('/', async (req,res) => {
+//     try{
+//         const commentData = await Comment.create(req.body);
+//         res.status(200).json(commentData);
+//     }catch(err){
+//         res.status(400).json(err);
+//     }
+// });
+
