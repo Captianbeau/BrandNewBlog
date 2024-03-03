c4d486b9b67dca35bb4f7af6064bbf69ca7c955d
Capti <captianbeau@gmail.com>
2024-03-03 14:26:23 -0700

    added base code to home routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+router.get('/', async (req,res) => {
+    try{
+        res.render('homepage');
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+router.get('/profile', async (req,res) => {
+    try{
+        res.render('profile');
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req,res) => {
+    res.render('login');
+});
+
+module.exports = router;
