3d4ccab8f9d14419e2bfd60fccc1b85b8cea631f
Capti <captianbeau@gmail.com>
2024-03-03 13:48:35 -0700

    added post to create User

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,13 +1,26 @@
 // post
 const router = require('express').Router();
-const{User} = require('../../models');
+const { User } = require('../../models');
 
-// router.post('/', async (req,res) => {
-//     try{
-//         const commentData = await Comment.create(req.body);
-//         res.status(200).json(commentData);
-//     }catch(err){
-//         res.status(400).json(err);
-//     }
-// });
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        })
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } })
+    }
+})
 
