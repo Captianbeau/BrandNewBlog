fff6dc6d505c1465f8f89960c94fd355139a37f3
Capti <captianbeau@gmail.com>
2024-03-03 13:55:29 -0700

    added post for login

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -20,7 +20,32 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { email: req.body.email } })
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
+        if(!userData){
+            res
+            .status(400)
+            .json({message: 'incorrect email or password'});
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if(!validPassword){
+            res
+            .status(400)
+            .json({ message: 'incorrect email or password'});
+            return;
+        }
+
+        req.session.save(()=>{
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({user: userData, message:'logged in'});
+        })
+    }catch(err){
+        res.status(400).json(err);
     }
 })
 
