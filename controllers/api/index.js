0eea40590a14ccd2f7c3604d11b49db623eb9d8a
Capti <captianbeau@gmail.com>
2024-03-03 13:58:35 -0700

    added post for logout and uncommented user route in index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const userRoute = require('./user.js');
 const postRoute = require('./post.js');
 const commentRoute = require('./comment.js');
 
-// router.use('/users', userRoute);
+router.use('/users', userRoute);
 router.use('/posts', postRoute);
 router.use('/comments', commentRoute);
 
